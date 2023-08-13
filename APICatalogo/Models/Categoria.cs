195ae92ce4b6e4766dbeeba80cde991b79fc79d4
RodrigoToledo5599 <rodrigotmt89@gmail.com>
2023-08-13 00:58:28 -0300

    Add project files.

---
+++ APICatalogo/Models/Categoria.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APICatalogo.Models
+{
+	public class Categoria
+	{
+		[Key]
+		public int CategoriaId { get; set; }
+        public string? Nome { get; set; }
+        public string? ImagemUrl { get; set; }
+		public ICollection<Produto>? Produtos { get; set; }
+	}
+}
